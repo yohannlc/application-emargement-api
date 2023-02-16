ca7f7866035ae7af18c2404892114d5ad8cffd1c
Clement YZIQUEL <clement.yziquel@isen-ouest.yncrea.fr>
2023-02-16 14:00:23 +0100

    Ajout d'une session

--- src/Controller/ApiSessionController.php
+++ src/Controller/ApiSessionController.php
@@ -6,12 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Validator\Constraints\DateTimeInterface;
+
 
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Nelmio\ApiDocBundle\Annotation\Security;
 use OpenApi\Annotations as OA;
 
 use App\Entity\Session;
+use App\Entity\Matiere;
+use App\Entity\Type;
+use App\Entity\Groupe;
+use App\Entity\Salle;
+use App\Entity\Staff;
+
+use DateTime;
 
 #[Route('/api/v1.0', name: 'api_')]
 class ApiSessionController extends AbstractController{
@@ -21,5 +32,93 @@ class ApiSessionController extends AbstractController{
         $this->doctrine = $doctrine;
     }
 
-    
+    /**
+     * Création d'une session
+     * 
+     * @OA\Response(
+     *    response=201,
+     *    description="Session créée"
+     * )
+     * 
+     * @OA\Response(
+     *   response=400,
+     *   description="Requete invalide"
+     * )
+     * 
+     * @OA\RequestBody(
+     *    @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(property="date", type="date"),
+     *       @OA\Property(property="heure_debut", type="string", format="time", example="08:00:00"),
+     *       @OA\Property(property="heure_fin", type="string", format="time", example="10:00:00"),
+     *       @OA\Property(property="id_matiere", type="integer"),
+     *       @OA\Property(property="type", type="string"),
+     *       @OA\Property(property="idGroupes", type="array", @OA\Items(type="integer")),
+     *       @OA\Property(property="idSalles", type="array", @OA\Items(type="integer")),
+     *       @OA\Property(property="idIntervenants", type="array", @OA\Items(type="integer"))
+     *    )
+     * )
+     * 
+     * @OA\Tag(name="Session")
+     */
+    #[Route('/session/create', name: 'create_session',methods: ['POST'])]
+    public function createSession(Request $request){
+        $entityManager = $this->doctrine->getManager();
+
+        $data = json_decode($request->getContent(), true);
+
+        $date = new DateTime($data['date']);
+        $heureDebut = new DateTime($data['heure_debut']);
+        $heureFin = new DateTime($data['heure_fin']);
+        $idMatiere = $entityManager->getRepository(Matiere::class)->find($data['id_matiere']);
+        $type = $entityManager->getRepository(Type::class)->find($data['type']);
+        $idGroupes = $data['idGroupes'];
+        $idSalles = $data['idSalles'];
+        $idIntervenants = $data['idIntervenants'];
+
+        $session = new Session();
+        if($date == null){
+            throw new BadRequestHttpException("La date n'est pas valide");
+        }elseif($heureDebut == null){
+            throw new BadRequestHttpException("L'heure de début n'est pas valide");
+        }elseif($heureFin == null){
+            throw new BadRequestHttpException("L'heure de fin n'est pas valide");
+        }elseif($idMatiere == null){
+            throw new BadRequestHttpException("La matière n'est pas valide");
+        }elseif($type == null){
+            throw new BadRequestHttpException("Le type n'est pas valide");
+        }elseif($idGroupes == null){
+            throw new BadRequestHttpException("Le groupe n'est pas valide");
+        }elseif($idSalles == null){
+            throw new BadRequestHttpException("La salle n'est pas valide");
+        }elseif($idIntervenants == null){
+            throw new BadRequestHttpException("L'intervenant n'est pas valide");
+        }else{
+            $session->setDate($date);
+            $session->setHeureDebut($heureDebut);
+            $session->setHeureFin($heureFin);
+            $session->setIdMatiere($idMatiere);
+            $session->setType($type);
+            foreach($idGroupes as $idGroupe){
+                $session->addIdGroupe($entityManager->getRepository(Groupe::class)->find($idGroupe));
+            }
+            foreach($idSalles as $idSalle){
+                $session->addIdSalle($entityManager->getRepository(Salle::class)->find($idSalle));
+            }
+            foreach($idIntervenants as $idIntervenant){
+                $session->addIdStaff($entityManager->getRepository(Staff::class)->find($idIntervenant));
+            }
+        }
+
+        $entityManager->persist($session);
+        $entityManager->flush();
+
+        $response = new Response();
+        $response->setStatusCode(Response::HTTP_CREATED);
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
+    }
+
+
 }
--- src/Entity/Session.php
+++ src/Entity/Session.php
@@ -240,18 +240,18 @@ class Session
         return $this->idStaff;
     }
 
-    public function addIdStaff(Staff $idStaff): self
+    public function addIdStaff(Staff $staff): self
     {
-        if (!$this->idStaff->contains($idStaff)) {
-            $this->idStaff->add($idStaff);
+        if (!$this->idStaff->contains($staff)) {
+            $this->idStaff->add($staff);
         }
 
         return $this;
     }
 
-    public function removeIdStaff(Staff $idStaff): self
+    public function removeIdStaff(Staff $staff): self
     {
-        $this->idStaff->removeElement($idStaff);
+        $this->idStaff->removeElement($staff);
 
         return $this;
     }
